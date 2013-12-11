fb41a46ec99e68b2119ca814b6e6cf8d73749f47
ethesx <ethesx@gmail.com>
2013-12-11 17:17:57 -0500

    Updated to handle loan request parameters

--- models/loan.js
+++ models/loan.js
@@ -4,15 +4,23 @@ var mongoose = require('mongoose')
 /* Catches all the loan data*/
 
 var LoanSchema = new Schema({
-  msrp: Number,
-  downPayment: Number,
-  tradeInValue: Number,
-  interestRate: Number,
-  term: Number,
-  termUnit: String,
-  salesTax: Number,
-  payment: Number,
-  paymentUnit: String
+  
+	reqLoan: Number,
+	reqTerm: Number,
+	reqTermUnit: String,			//TODO create constants
+	reqInterestRate: Number,
+	reqStartDateYear: Number, 		//TODO possibly change to date, calculate based on form values
+	reqStartDateMonth: String, 		//TODO possibly change to date, calculate based on form values
+	reqStartDateDay: Number, 		//TODO possibly change to date, calculate based on form values
+	msrp: Number,
+	downPayment: Number,
+	tradeInValue: Number,
+	interestRate: Number,
+	term: Number,
+	termUnit: String,
+	salesTax: Number,
+	payment: Number,
+	paymentUnit: String
 });
 
 mongoose.model('Loan', LoanSchema);
