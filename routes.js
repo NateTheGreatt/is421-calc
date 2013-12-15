f6c4cb62a572cdc09947949804d5fec78dbc9efd
ethesx <ethesx@gmail.com>
2013-12-14 23:06:49 -0500

    Moved controller statements to ./routes.js from controllers/router.js

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 module.exports = function(app){
-
+	
+	/*Controllers setup
+     * */
     var index = require('./controllers/index');
     var passport = require('passport');
     var users = require('./controllers/users');
     var frontend = require('./controllers/frontend');
-
+    var requests = require('./requests');
 
     app.get('/users/', users.list);
     app.get('/api/users', users.jsonlist);
@@ -28,6 +30,7 @@ module.exports = function(app){
     app.get('/myQalc', frontend.myQalc);
     app.get('/offerF', frontend.offerF);
     app.get('/qalc', frontend.qalc);
+    app.post('/createQalc', requests.addLoanRequest);
 
     function ensureAuthenticated(req, res, next) {
       if (req.isAuthenticated()) { return next(); }
