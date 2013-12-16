63b9dca0de3cb09d3e9663c4a926f25d738b4582
NateTheGreatt <nam23@njit.edu>
2013-12-15 19:37:16 -0500

    server pushes fake data for offers page request

--- controllers/index.js
+++ controllers/index.js
@@ -20,13 +20,22 @@ exports.login = function(req, res) {
 };
 
 exports.myQalcs = function(req, res) {
-    var o = {qalcs: [
+    var qalcs = {qalcs: [
         {name: "Qalc A", description: "fake desc", quote: 15000, interest: 0.6, months: 48, monthly: 352.28},
         {name: "Qalc B", description: "fake desc", quote: 1000, interest: 0.52, months: 24, monthly: 552.28},
         {name: "Qalc C", description: "fake desc", quote: 5000, interest: 0.49, months: 32, monthly: 452.28}
     ]};
-    res.render('myQalcs', o);
+    res.render('myQalcs', qalcs);
 };
+
+exports.offers = function(req,res) {
+    var qalcs = {qalcs: [
+        {name: "Qalc A", description: "fake desc", quote: 15000, interest: 0.6, months: 48, monthly: 352.28},
+        {name: "Qalc B", description: "fake desc", quote: 1000, interest: 0.52, months: 24, monthly: 552.28},
+        {name: "Qalc C", description: "fake desc", quote: 5000, interest: 0.49, months: 32, monthly: 452.28}
+    ]};
+    res.render('offers', qalcs);
+}
 
 exports.getOffer = function(req, res) {
     var id = req.params.id;
