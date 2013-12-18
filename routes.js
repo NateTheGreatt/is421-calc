2ea9cf024eeb39c7849b84a679e811a00a6871a3
jamesperks <tail451@gmail.com>
2013-12-18 18:52:44 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,43 +1,53 @@
 module.exports = function(app){
+	
+	/*Controllers setup
+     * */
+    var index = require('./controllers/index');
+    var passport = require('passport');
+    var users = require('./controllers/users');
+    var requests = require('./controllers/requests');
+    var upload = require('./controllers/upload');
 
+    /* app.get('/', index.index);
+      app.get('/admin',ensureAuthenticated , admin.main);
+    */
 
+    app.get('/', index.index);
+    app.get('/qalcSelect', index.qalcSelect);
+    app.get('/login', index.login); app.get('/myQalcs', index.myQalcs);
+    app.get('/offers', index.offers);
+    app.get('/bankOffer', index.bankOffer);
 
-var index = require('./controllers/index');
-var passport = require('passport');
-var users = require('./controllers/users');
-var admin = require('./controllers/admin');
+    app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), users.auth);
+    app.post('/createQalc', requests.addLoanRequest);
 
-/* app.get('/', index.index);
-  app.get('/admin',ensureAuthenticated , admin.main); 
-*/
-  
-  app.get('/users/', users.list);
-  app.get('/api/users', users.jsonlist);
-  app.get('/users/:uid', users.findById);
-  app.post('/users/add', users.add);
-  app.post('/users/remove', users.remove);
-  app.post('/users/update/:uid', users.update);
-  app.get('/login', users.login);
-//  app.post('/login', passport.authenticate('local'), users.auth);
-  app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), users.auth);
-  app.get('/api', function (req,res) {
-    var obj = {
-     spam: 'test'
+    app.get('/api', function (req,res) {
+        var obj = {
+         spam: 'test'
+        };
+        res.send(obj);
+    });
+
+     //app.get('/myQalcs', index.myQalcs);
+    app.get('/myQalcs', requests.getCalcs);
+   	app.get('/qalc', requests.getLoanCalc);
+   	app.post('/createQalc', requests.addLoanRequest);
+
+    /*app.get('/qalcSelect', frontend.qalcSelect);
+    app.get('/login', frontend.login);
+    app.get('/myQalc', frontend.myQalc);
+    app.get('/offerF', frontend.offerF);
+    app.get('/qalc', frontend.qalc);
+    app.get('/users', frontend.users);*/
+    
+    app.get('/upload', upload.uploadForm);
+    app.post('/upload', upload.upload);
+    app.get('/viewfiles', upload.viewFiles);
+
+    function ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) { return next(); }
+         res.cookie('lasturl', req.path);
+         res.redirect('/login');
     }
-    res.send(obj);
-  });
-  
-/*function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next();}
-  else {res.redirect('/login')};
-     res.cookie('lasturl', req.path);
-     res.redirect('/login'); 
-*/
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-     res.cookie('lasturl', req.path);
-     res.redirect('/login');
-}
-
-}
+};
