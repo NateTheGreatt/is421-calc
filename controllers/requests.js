ae23e2b92d6c5e1beaaeb2ca5e363a05d0e01f8c
ethesx <ethesx@gmail.com>
2013-12-17 21:52:12 -0500

    Added constant and references to views directory Added exports getLoanCalc() for inital population of calc data Updated exports addLoanRequest(): 	model field reference names 	Added use of Request model to collect data prior to passing to findOneAndUpdate 	Changed Request.update to findOneAndUpdate to return document for population in updated page Updated exports addLoanResponse() callback to handle render async proper Updated resultLogCallback signature for callback use Updated populateCalc() signature to accept response object for render handling

--- controllers/requests.js
+++ controllers/requests.js
@@ -8,6 +8,26 @@ var mongoose = require('mongoose'),
 	LoanResponse = mongoose.model('LoanResponse'),
 	CommonRequest = mongoose.model('CommonRequest');
 
+var viewsDir = "..\\views";
+
+
+exports.getLoanCalc = function(req, res){
+	Request.findOne(
+			{userId : "chico",
+			calcType : "loan"},
+			"status calcType req_obj common",
+			null,
+			function(err, request){
+				console.log("REQUEST = " + request);
+				
+				res.render(
+						viewsDir + req.path,
+						{request : request});
+			}
+	);
+	
+};
+
 // Insert or update a request
 exports.addLoanRequest = function(req, res) {
 	var data = req.body;
@@ -15,15 +35,15 @@ exports.addLoanRequest = function(req, res) {
 	console.log(data);
 	
 	var loanObj = new LoanRequest({
-		reqLoan : data.loan,
-		reqTerm : data.term,
-		reqTermUnit : data.termUnit,
-		reqInterestRate : data.snterestRate,
-		reqStartDateYear : data.startDateYear,
-		reqStartDateMonth : data.startDateMonth,
-		reqStartDateDay : data.startDateDay
+		loan : data.loan,
+		term : data.term,
+		termUnit : data.termUnit,
+		interestRate : data.interestRate,
+		startDateYear : data.startDateYear,
+		startDateMonth : data.startDateMonth,
+		startDateDay : data.startDateDay
 	});
-
+	
 	var commonObj = new CommonRequest({
 		
 		name : data.name,	//TODO placeholder
@@ -32,22 +52,39 @@ exports.addLoanRequest = function(req, res) {
 		audio : data.audio,	//TODO placeholder
 		comment : data.comment
 	});
-
-	Request.update({
-		userId : "chico",	// TODO dynamically assign user
-		calcType : "loan"
-	}, 
-	{
+	
+	var request = new Request({
 		status : "draft",	// TODO status field - define constants. draft, submitted, reviewed
 		calcType : "loan",	// TODO calcType field - define constants. lease, loan...
 		req_obj : loanObj.toObject(),
 		common : commonObj.toObject()
-	}, {
+		
+	});
+	
+	request = request.toObject();
+	delete request._id;
+	console.log("Request object to be saved : ");
+	console.log(request);
+	
+	Request.findOneAndUpdate({
+		userId : "chico",	// TODO dynamically assign user
+		calcType : "loan"
+	}, 
+	request
+	, {
 		safe : true,
 		upsert : true
-	},	resultLogCallback);
+	},	function(err, request){
+				console.log("Request object returned : ");
+				console.log(request.toJSON());
+				
+				res.render(
+					viewsDir + "\\qalc",	//TODO replace with dynamic reference
+					{request : request});
+
+		});
 	
-	res.render('../views/qalc');	//TODO replace with dynamic reference
+	
 };
 
 // Insert or update a new response for a request
@@ -77,9 +114,16 @@ exports.addLoanResponse = function(req, res) {
 	}, {
 		safe : true,
 		upsert : true
-	}, resultLogCallback);
-	
-	res.render('../views/bankOffer');	//TODO replace with dynamic reference
+	}, function(err, numberAffected, raw){
+		resultLogCallback(
+				err,
+				numberAffected,
+				raw,
+				function(){
+					res.render('../views/bankOffer');	//TODO replace with dynamic reference
+				});
+		}
+	);
 };
 
 // Insert or update a new response for a request
@@ -90,24 +134,37 @@ exports.getCalcs = function(req, res) {
         	userId : "chico",	// TODO dynamically assign user
         	calcType : "loan"},	
         	"calcType req_obj common",
-        	populateCalc);
+        	null,
+        	function(err, doc){
+        		populateCalc(err, doc, res);
+        	});
 
-	res.render(req.path);
 };
 
 //Populate object for return to myCalcs page. Assumes loan based on page constraints!
-function populateCalc(err, request){
-	var calc = {
-		name : request.common.name,
-		description : request.common.description,
-		quote : request.req_obj.loan,
-		interest : req_obj.interestRate,
-		term : request.req_obj.term,
-		termUnit : request.req_obj.termUnit,
-		monthly : getPayment(request.req_obj.loan, request.term, request.rate, request.termUnit)
-	};
+function populateCalc(err, request, res){
 	
+	var calc = [];
+	if(request != ""){
+		console.log("ERR is : " + err +
+		" request is : " + request +
+		" res is : " + res);
+		
+		calc.push({
+			name : request.common.name,
+			description : request.common.description,
+			quote : request.req_obj.loan,
+			interest : req_obj.interestRate,
+			term : request.req_obj.term,
+			termUnit : request.req_obj.termUnit,
+			monthly : getPayment(request.req_obj.loan, request.term, request.rate, request.termUnit)
+		});
+	}
 	
+	res.render(
+		viewsDir + req.path, 
+		calc
+	);
 }
 
 
@@ -131,13 +188,13 @@ function convertTerm(term, termUnit){
 			return term * 12;
 	}
 }
-function resultLogCallback(err, numberAffected, raw) {
+function resultLogCallback(err, numberAffected, raw, callback) {
 
 	if (err)
 		return handleError(err);
 	console.log('The number of updated documents was %d', numberAffected);
 	console.log('The raw response from Mongo was ', raw);
-	
+	callback();
 }
 
 function handleError(err) {
