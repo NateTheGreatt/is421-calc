10cc068573e9d1b291f3fcae44643820ba5fa5f5
ethesx <ethesx@gmail.com>
2013-12-18 22:14:33 -0500

    Revert "Correct bug. Callback missing additional req argument "
    This reverts commit e5cc7ac56a1ac8d3c17dc0e0b34919c1f9df64b8.

--- controllers/requests.js
+++ controllers/requests.js
@@ -17,7 +17,7 @@ exports.getLoanCalc = function(req, res){
 			calcType : "loan"},
 			"status calcType req_obj common",
 			null,
-			function(err, request, req){
+			function(err, request){
 				console.log("REQUEST = " + request);
 				
 				res.render(
