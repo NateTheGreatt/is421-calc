4b9340638fdf1a01afc0aae3639fd0744ade126b
ethesx <ethesx@gmail.com>
2013-12-18 21:40:01 -0500

    Removed offerF control. Handled by index controller with offers template

--- routes.js
+++ routes.js
@@ -37,7 +37,6 @@ module.exports = function(app){
    	app.post('/createQalc', requests.addLoanRequest);
 
    	//The below control to be phased out
-    app.get('/offerF', frontend.offerF);
     app.get('/qalc', frontend.qalc);
     
 
